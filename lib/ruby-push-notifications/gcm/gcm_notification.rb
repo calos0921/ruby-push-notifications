0b47161c5bca2b8df528bf49dd652bf5cb104ba6
calos0921 <36837151+calos0921@users.noreply.github.com>
2018-11-13 22:10:12 +0800

    Change to FCM format.

--- lib/ruby-push-notifications/gcm/gcm_notification.rb
+++ lib/ruby-push-notifications/gcm/gcm_notification.rb
@@ -15,15 +15,14 @@ module RubyPushNotifications
       # @param [Hash]. Payload to send.
       def initialize(registration_ids, data)
         @registration_ids = registration_ids
-        @data = data
+        @json_data = {registration_ids: @registration_ids}.merge(data)
       end
 
       # @return [String]. The GCM's JSON format for the payload to send.
       #    (https://developer.android.com/google/gcm/server-ref.html#send-downstream)
       def as_gcm_json
         JSON.dump(
-          registration_ids: @registration_ids,
-          data: @data
+          @json_data
         )
       end
     end
